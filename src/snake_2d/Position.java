94c29befe28c1abf5b7a2cba682ff50239593e36
HusseinAzghir <husain.azghir@gmail.com>
2023-12-07 12:27:14 +0100

    Snake/Schlange

---
+++ src/snake_2d/Position.java
@@ -0,0 +1,15 @@
+/*  harmash.com  هذا الكود مشروح بتفصيل في موقع
+    CopyRight © 2017 harmash.com - All Rights Reserved - developed by Mhamad Harmush
+*/
+package snake_2d;
+ 
+public class Position {
+ 
+    int x, y;
+ 
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+ 
+}
